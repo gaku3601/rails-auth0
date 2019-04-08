d5b89279048535966449923b2037fba3ed1fb1b2
gaku3601 <pro.gaku@gmail.com>
2019-04-08 16:54:06 +0900

    認証機能の実装

---
+++ app/app/controllers/concerns/secured.rb
@@ -0,0 +1,41 @@
+module Secured
+    extend ActiveSupport::Concern
+  
+    SCOPES = {
+      '/api/private-scoped' => ['read:messages'] # scopeが必要なrequestはこのように記載
+    }
+  
+    included do
+      before_action :authenticate_request!
+    end
+  
+    private
+  
+    def authenticate_request!
+      @auth_payload, @auth_header = auth_token
+  
+      render json: { errors: ['Insufficient scope'] }, status: :forbidden unless scope_included
+    rescue JWT::VerificationError, JWT::DecodeError
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+    end
+  
+    def http_token
+      if request.headers['Authorization'].present?
+        request.headers['Authorization'].split(' ').last
+      end
+    end
+  
+    def auth_token
+      JsonWebToken.verify(http_token)
+    end
+  
+    def scope_included
+      # The intersection of the scopes included in the given JWT and the ones in the SCOPES hash needed to access
+      # the PATH_INFO, should contain at least one element
+      if SCOPES[request.env['PATH_INFO']] == nil
+        true
+      else
+        (String(@auth_payload['scope']).split(' ') & (SCOPES[request.env['PATH_INFO']])).any?
+      end
+    end
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'api/private' => 'private#private'
+  get 'api/private-scoped' => 'private#private_scoped'
 end
